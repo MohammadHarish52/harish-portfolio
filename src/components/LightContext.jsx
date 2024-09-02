66daddb6aa93e41c3a1638eb25fa1d90c94c62f5
tachyonok <tachyonparticle123@gmail.com>
2024-09-02 23:11:58 +0530

    kept dark as bg

--- src/components/LightContext.jsx
+++ src/components/LightContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useState } from "react";
 export const LightContext = createContext();
 
 export const LightProvider = ({ children }) => {
-  const [light, setLight] = useState(false);
+  const [light, setLight] = useState(true);
 
   return (
     <LightContext.Provider value={{ light, setLight }}>
